8498cce3b480ca3eea735cac5e3e105d620aa9b4
faithyyyy <faithfortune6@gmail.com>
2023-03-17 20:04:13 +0100

    Signin and signout functionality

--- src/Auth/Firebase.jsx
+++ src/Auth/Firebase.jsx
@@ -1,14 +1,13 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_KEYS,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTHDOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECTID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGEBUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGINGSENDERID,
-  appId: import.meta.env.VITE_FIREBASE_APPID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENTID,
+  apiKey: "AIzaSyDIsoRKkgaKbBaaW9m_IP-8ipxF-19m0uk",
+  authDomain: "xoxo-db.firebaseapp.com",
+  projectId: "xoxo-db",
+  storageBucket: "xoxo-db.appspot.com",
+  messagingSenderId: "648019716902",
+  appId: "1:648019716902:web:bf83448bf3f9b3e689afe7",
+  measurementId: "G-XZH40XX5RY",
 };
-
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
