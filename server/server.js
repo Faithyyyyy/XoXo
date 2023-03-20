a2d7251ed45249711bfa1ea8b88998e3a2c86a62
faithyyyy <faithfortune6@gmail.com>
2023-03-20 18:43:33 +0100

    Implemented payment with stripe

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ app.use(express.static("public"));
 // app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://xoxoapp.netlify.app/"],
+    // origin: ["http://localhost:5173", "https://xoxoapp.netlify.app/"],
   })
 );
 
