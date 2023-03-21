4e64a287f6b934d247dc9605cdf1ca1337e29715
faithyyyy <faithfortune6@gmail.com>
2023-03-21 03:05:45 +0100

    Implemented payment with stripe

--- server/server.js
+++ server/server.js
@@ -1,14 +1,12 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-// const bodyParser = require("body-parser");
 require("dotenv").config();
 const port = process.env.PORT;
 const Stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 app.use(express.json());
 app.use(express.static("public"));
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use(cors());
 // origin: ["http://localhost:5173", "https://xoxoapp.netlify.app/"],
 
@@ -44,35 +42,7 @@ app.post("/create-checkout-session", async (req, res) => {
   } catch (e) {
     res.status(500).json({ error: e.message });
   }
-  // res.json({ url: "Hi" });
 });
-// app.post("/pay", async (req, res) => {
-//   console.log(req.body.token);
-//   await Stripe.charges.create({
-//     source: req.body.token.id,
-//     amount: req.body.amount,
-//     currency: "usd",
-//   });
-// });
-
-// Router.post('/create-checkout-session', async (req, res) => {
-//   const session = await stripe.checkout.sessions.create({
-//     line_items: [
-//       {
-//         // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
-//         price: '{{PRICE_ID}}',
-//         quantity: 1,
-//       },
-//     ],
-//     mode: 'payment',
-//     success_url: `${process.env.CLIENT_URL}/checkout-success`,
-//     cancel_url: `${process.env.CLIENT_URL}/cart`,
-//   });
-
-//   res.send({url: session.url});
-// });
-
-// module.exports = Router
 
 app.listen(3000, () => {
   console.log(`Server is running on ${3000}`);
